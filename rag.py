0c82f4bcaa8540786f2d1c00a254500a31cc425e
sal-git-hub <saleha.college@gmail.com>
2026-08-02 21:25:58 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,13 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+
 from transformers import pipeline
-from langchain.llms import HuggingFacePipeline
+from langchain_huggingface import HuggingFacePipeline
 
 
 def load_documents(pdf_paths):
-
     documents = []
 
     for pdf in pdf_paths:
@@ -18,7 +18,6 @@ def load_documents(pdf_paths):
 
 
 def split_documents(documents):
-
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
@@ -28,26 +27,24 @@ def split_documents(documents):
 
 
 def create_embeddings():
-
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2",
-        model_kwargs={"device": "cpu"}
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
 
 def create_vector_store(chunks):
-
     embeddings = create_embeddings()
-
     return FAISS.from_documents(chunks, embeddings)
 
 
 def load_llm():
-
-    pipe = pipeline(
-        task="text2text-generation",
+    generator = pipeline(
+        "text2text-generation",
         model="google/flan-t5-base",
-        max_new_tokens=256
+        max_new_tokens=256,
+        device=-1
     )
 
-    return HuggingFacePipeline(pipeline=pipe)
+    return HuggingFacePipeline(
+        pipeline=generator
+    )
